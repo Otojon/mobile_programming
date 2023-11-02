e2bffa91c82775f61b3120224cf1bd93f28d7b15
otojon_khudayarov <2110075@newuzbekistanuniversity.uz>
2023-11-02 16:29:51 +0500

    lab 3 => done 7 th problem

--- LAB3/main.dart
+++ LAB3/main.dart
@@ -23,6 +23,7 @@ void main() {
   print("Hello My Name is Otojon . I'm  19 years old")
 }
 */
+// ----------------------------------------------------------------
 
 //2. Write a program in which you check whether a number is odd or even
 // ----------------------------------------------------------------
@@ -49,3 +50,103 @@ void main() {
   }
 }
 */
+// ----------------------------------------------------------------
+
+//3. Write a program to print all odd/even numbers up to 100.
+// ----------------------------------------------------------------
+/*void main() {
+  for (int i = 0; i <= 100; i++) {
+    if (i % 2 == 0) {
+        print('$i is even.');
+      } else {
+        print('$i is odd.');
+      }
+  }
+}
+*/
+// ----------------------------------------------------------------
+
+// 4. Given a List of integer numbers: (List<int> a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];) save and create another List of integers which squares all the elements of the first List.
+// Print the result.
+// ----------------------------------------------------------------
+/*
+void main() {
+  List<int> a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+  List<int> b = [];
+  for (int i = 0; i < a.length; i++)
+  b.add(a[i]*a[i]);
+  print(b);
+}
+*/
+// ----------------------------------------------------------------
+
+// Problem 5:
+// Write a code in Dart in which you determine all divisors of a given number and print them in
+// increasing order. (for instance: given 4, print 4, 2 → exclude 1)
+// ----------------------------------------------------------------
+/*import 'dart:io';
+
+void main() {
+  print('Enter a number:');
+
+  String? userInput = stdin.readLineSync();
+  if (userInput != null) {
+    int number = int.parse(userInput);
+    print('Divisors of $number (excluding 1):');
+    for (int i = 2; i <= number; i++) {
+      if (number % i == 0) {
+        print(i);
+      }
+    }
+  }
+}
+*/
+// ----------------------------------------------------------------
+
+// Problem 6:
+// Given two Lists, you need to find matching elements between them and save them in another
+// list and print their result:
+// The two lists:
+// List<int> a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+// List<int> b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 89];
+// ----------------------------------------------------------------
+/*void main() {
+  List<int> a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+  List<int> b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 89];
+  List<int> c = [];
+  for (int Na1 in a) {
+    if (b.contains(Na1) && !c.contains(Na1)) {
+      c.add(Na1);
+    }
+  }
+  print(c);
+}
+*/
+// ----------------------------------------------------------------
+
+// Problem 7:
+// Write a program that determines whether a given number is Prime or not.
+// ----------------------------------------------------------------
+
+/*import 'dart:io';
+
+void main() {
+  List<int> a = [];
+  print('Enter a number:');
+  String? userInput = stdin.readLineSync();
+  if (userInput != null) {
+    int number = int.parse(userInput);
+    for (int i = 2; i <= number; i++) {
+      if (number % i == 0) {
+        a.add(i);
+      }
+    }
+    if (a.length > 1) {
+      print('it is not a prime number');
+    } else {
+      print('it is a prime number');
+    }
+  }
+}
+*/
+// ----------------------------------------------------------------
